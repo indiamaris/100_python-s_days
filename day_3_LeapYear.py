4fc82b7d7c67daefc380bc8c90f21b4adef92501
MsC. Indiamaris <indiamaris@gmail.com>
2022-06-21 18:59:22 +0200

    Create day_3_LeapYear.py

---
+++ day_3_LeapYear.py
@@ -0,0 +1,7 @@
+year = int(input('Which year do you want to check?'))
+if (year % 4 == 0) & (year % 100 != 0):
+    print('Leap year.')
+elif (year % 4 == 0) & (year % 100 == 0) & (year % 400 == 0):
+    print('Leap year.')
+else:
+    print('Not leap year.')
